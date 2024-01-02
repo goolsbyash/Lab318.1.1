33c48a67085036d9f4906d99d2b036c489fd8494
goolsbyash <goolsbyashley015@gmail.com>
2024-01-02 14:53:18 -0500

    Part Five completed.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 console.log("It's fun learning how to use Node.js!");
 
-// Part Four: Creating a Server
+//* Part Four: Creating a Server
 
+// First server example
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -18,14 +19,50 @@ const port = 3000;
 //     console.log(`Server running at http://${hostname}:${port}/`);
 //   });
 
-
 // Second server example
+
+// const server = http
+//   .createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/html");
+//     res.write('<h1 style="color: red">Hello World!</h1>');
+//     res.write("<p>I wonder what else we can send...</p>");
+//     res.end();
+//   });
+
+//* Part Five: Open Exploration
+
 const server = http
   .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write('<h1 style="color: red">Hello World!</h1>');
-    res.write("<p>I wonder what else we can send...</p>");
-    res.end();
+    if (req.url === "/") {
+      res.status = 200; //status code ok
+      res.setHeader("Content-Type", "text/html");
+      res.write(
+        '<h1 style="color: purple; text-align: center">Welcome to My First Web App with Node.js</h1>'
+      );
+      res.write(
+        '<nav style="display: flex; flex-direction: row; justify-content: space-between"> <a href="/">Home </a><a href="/about">About Me </a><a href="/gallery">Gallery </a></nav>'
+      );
+      res.end(); // concludes the request
+    } else if (req.url === "/about") {
+      res.status = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.write('<h1 style="color: purple; text-align: center"> About Me</h1>');
+      res.write(
+        '<h2 style="text-align: center"> My name is Ashley Goolsby and I am an evolving junior full stack developer. </h2>'
+      );
+      res.end();
+    } else if (req.url === "/gallery") {
+      res.status = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.write('<h1 style="text-align: center"> Gallery </h1>');
+      res.end();
+    } else {
+      res.status = 404;
+      res.write("<h2> Status 404: Page Not Found</h2>");
+      res.end();
+    }
   })
-  
+  .listen(port, hostname, () => {
+    console.log(`Local server running at http://${hostname}:${port}/`);
+  });
