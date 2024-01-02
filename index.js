50dbf2e0725721ddde0f0a91ec8cf7645a040c00
goolsbyash <goolsbyashley015@gmail.com>
2024-01-02 13:42:13 -0500

    Part Four completed.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 // console.log('Hello World!');
 
 console.log("It's fun learning how to use Node.js!");
+
+// Part Four: Creating a Server
+
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// const server = http
+//   .createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("Hello World!\n");
+//   })
+//   .listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+//   });
+
+
+// Second server example
+const server = http
+  .createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write("<p>I wonder what else we can send...</p>");
+    res.end();
+  })
+  
